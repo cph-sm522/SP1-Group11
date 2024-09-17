60cb3e45d6b26187becef2694ae70e051b339dd2
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-09-17 13:46:06 +0200

    FETCH DATA + Entitites fixed + relations created

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -15,7 +15,7 @@ import java.net.http.HttpResponse;
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
         // EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("persons");
-        String apiKey = System.getenv("api_key");
+        String apiKey = System.getenv("api_key"); // HUSK ÆNDRING AF API NAVN
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2019-01-01&sort_by=popularity.desc&with_origin_country=DK&api_key=" + apiKey))
