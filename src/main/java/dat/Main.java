c64178a1ed703980678a91e65ad36dd29707766f
Tobitastik <144216903+Tobitastik@users.noreply.github.com>
2024-09-19 15:46:10 +0200

    Loading multiple pages. Some actors and director have no data and will be using the createDefaultActor / createDefaultDirector to save the movie to the database. Some movies are still not included.

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -5,14 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dat.config.HibernateConfig;
 import dat.dtos.ActorDTO;
 import dat.dtos.DirectorDTO;
-import dat.dtos.MovieDTO;
-import dat.entities.Actor;
-import dat.entities.Director;
 import dat.entities.Movie;
 import dat.services.ActorService;
 import dat.services.DirectorService;
 import dat.services.MovieService;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.io.IOException;
@@ -33,34 +29,53 @@ public class Main {
             throw new IllegalStateException("API key is not set. Please set the 'api_key' environment variable.");
         }
 
-        String responseBody = fetchMoviesFromAPI(apiKey);
-        if (responseBody != null) {
-            persistDataToDB(responseBody, emf);
-        }
+        // Fetch and persist movies from all pages of the API
+        fetchAndPersistMovies(apiKey, emf);
     }
 
-    public static String fetchMoviesFromAPI(String apiKey) throws URISyntaxException, IOException, InterruptedException {
+    public static void fetchAndPersistMovies(String apiKey, EntityManagerFactory emf) throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2019-01-01&sort_by=popularity.desc&with_origin_country=DK&api_key=" + apiKey))
-                .GET()
-                .build();
+        int totalPages = 1;  // Initialize to 1, will be updated later
+        int currentPage = 1;
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() == 200) {
+        while (currentPage <= totalPages) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=" + currentPage + "&primary_release_date.gte=2019-01-01&sort_by=popularity.desc&with_origin_country=DK&api_key=" + apiKey))
+                    .GET()
+                    .build();
 
-            System.out.println(response.body());
-            return response.body();
-        } else {
-            System.out.println("Request failed: " + response.statusCode());
-            return null;
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                String responseBody = response.body();
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonResponse = objectMapper.readTree(responseBody);
+
+                // Persist movie data to the database
+                persistDataToDB(jsonResponse.get("results"), emf);
+
+                // Update total pages based on API response
+                totalPages = jsonResponse.get("total_pages").asInt();
+                System.out.println("Current Page: " + currentPage + " / " + totalPages);
+
+                currentPage++;
+            } else {
+                System.out.println("Request failed: " + response.statusCode());
+                break; // Exit loop on request failure
+            }
         }
     }
 
-    public static void persistDataToDB(String responseBody, EntityManagerFactory emf) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode moviesNode = objectMapper.readTree(responseBody).get("results");
+    private static int missingDataIdCounter = 1;
 
+    public static ActorDTO createDefaultActor() {
+        return new ActorDTO(missingDataIdCounter++, "no data");
+    }
+
+    public static DirectorDTO createDefaultDirector() {
+        return new DirectorDTO(missingDataIdCounter++, "no data", "no job");
+    }
+
+    public static void persistDataToDB(JsonNode moviesNode, EntityManagerFactory emf) {
         try (var entityManager = emf.createEntityManager()) {
             var transaction = entityManager.getTransaction();
             transaction.begin();
@@ -78,9 +93,19 @@ public class Main {
                     // Select only the first genre
                     Movie.Genre genre = Movie.Genre.fromId(movieNode.get("genre_ids").get(0).asInt());
 
+                    // Fetch director and actor data
                     DirectorDTO director = DirectorService.getDirectorInfo(id);
-                    Set<ActorDTO> actors = ActorService.getActors(id);
+                    if (director == null) {
+                        director = createDefaultDirector();
+                    }
 
+                    Set<ActorDTO> actors = ActorService.getActors(id);
+                    if (actors == null || actors.isEmpty()) {
+                        actors = new HashSet<>();
+                        actors.add(createDefaultActor());
+                    }
+
+                    // Persist movie using MovieService
                     MovieService.createMovie(id, title, releaseDate, genre, rating, overview, director, actors, emf);
 
                 } catch (Exception e) {
